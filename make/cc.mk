aab1416380f389988b94f83166a1663e05351bf1
André van Schoubroeck <andre@blaatschaap.be>
2022-03-12 22:16:07 +0100

    update compiler flags

--- make/cc.mk
+++ make/cc.mk
@@ -12,14 +12,25 @@ ifeq ($(COMPILER),CLANG)
 endif
 endif
 
+CXXFLAGS += -Werror
+CXXFLAGS += -Wall
+CXXFLAGS += --std=gnu++23
 
+CFLAGS += -Werror
+CFLAGS += -Wall
+CFLAGS += --std=gnu2x
 
 ifeq ($(BUILD_TYPE),DEBUG)
 	CXXFLAGS += -g  # Generate debug information (inside the binary)
 	CXXFLAGS += -O0 # Disable optimisation
+	CFLAGS += -g  # Generate debug information (inside the binary)
+	CFLAGS += -O0 # Disable optimisation
+e
 else 
 	CXXFLAGS += -O2 # Optimise code
 	CXXFLAGS += -D_FORTIFY_SOURCE=2
+	CFLAGS += -O2 # Optimise code
+	CFLAGS += -D_FORTIFY_SOURCE=2
 endif
 
 
--- make/lib.mk
+++ make/lib.mk
@@ -1,5 +1,5 @@
-LDFLAGS += 	$(shell $(PREFIX)pkg-config --libs   sdl)
-INCLUDES += $(shell $(PREFIX)pkg-config --cflags sdl)
+LDFLAGS += 	$(shell $(PREFIX)pkg-config --libs   sdl2)
+INCLUDES += $(shell $(PREFIX)pkg-config --cflags sdl2)
 
 LDFLAGS += 	$(shell $(PREFIX)pkg-config --libs   libpng)
 INCLUDES += $(shell $(PREFIX)pkg-config --cflags libpng)
