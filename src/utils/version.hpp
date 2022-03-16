5e6542cdceea64c215a3f5c3ffa43a962b25f766
André van Schoubroeck <andre@blaatschaap.be>
2022-03-16 15:37:08 +0100

    Compiler names

--- src/utils/version.hpp
+++ src/utils/version.hpp
@@ -57,21 +57,21 @@ class Version {
     const uint32_t m_compiler_major = __clang_major__;
     const uint32_t m_compiler_minor = __clang_minor__;
     const uint32_t m_compiler_patch = __clang_patchlevel__;
-    const std::string m_compiler_name = "Clang";
+    const std::string m_compiler_name = "C language family frontend for LLVM (Clang)";
     // __clang_version__	// string
 #elif __GNUC__
     // https://gcc.gnu.org/onlinedocs/cpp/Common-Predefined-Macros.html
     const uint32_t m_compiler_major = __GNUC__;
     const uint32_t m_compiler_minor = __GNUC_MINOR__;
     const uint32_t m_compiler_patch = __GNUC_PATCHLEVEL__;
-    const std::string m_compiler_name = "GCC";
+    const std::string m_compiler_name = "The GNU Compiler Collection (GCC)";
     // __VERSION__			// string
 #elif _MSC_FULL_VER
     //  https://docs.microsoft.com/en-us/cpp/preprocessor/predefined-macros
     const uint32_t m_compiler_major = (_MSC_FULL_VER / 10000000);
     const uint32_t m_compiler_minor = ((_MSC_FULL_VER / 100000) % 100);
     const uint32_t m_compiler_patch = (_MSC_FULL_VER % 100000);
-    const std::string m_compiler_name = "MSC";
+    const std::string m_compiler_name = "Microsoft C/C++ Optimizing Compiler (CL)";
 #endif
 
     const std::string m_compiler_full_name = m_compiler_name + " " + std::to_string(m_compiler_major) + "." +
