7559a0b8b50c8b4c04bb8bc087726d1f7f6dbaa7
Steven Wokke <git.desktop@stevenw.nl>
2022-03-25 18:44:52 +0100

    Grid improvements

--- src/types/grid.hpp
+++ src/types/grid.hpp
@@ -21,14 +21,24 @@ template <std::semiregular T> class Grid {
     T operator[](Point p) const { return m_elems[index(p)]; }
     T &operator[](Point p) { return m_elems[index(p)]; }
 
-    size_t index(Point p) {
+    size_t index(Point p) const {
         assert(p.x >= 0 && p.y >= 0 && p.x < m_width && p.y < m_height);
         return static_cast<size_t>(p.x) + static_cast<size_t>(p.y) * static_cast<size_t>(m_width);
     }
+    Point index_inverse(size_t s) const {
+        assert(s < m_width * m_height);
+        return {
+            static_cast<size_t>(s % m_width),
+            static_cast<size_t>(s / m_width)
+        };
+    }
 
     size_t size() const { return static_cast<size_t>(m_width) * static_cast<size_t>(m_height); }
-    std::span<T> span() const { return std::span(&m_elems[0], size()); }
-    // TODO: Key-value-range
+    std::span<const T> span() const { return std::span(&m_elems[0], size()); }
+    std::span<T> span() { return std::span(&m_elems[0], size()); }
+
+    // TODO: Add iter() and iter_mut() functions when c++ supports coroutines well.
+    // Currently (C++20), there is cppcoro::generator but that should have been part of the stl.
 
   private:
     GridSize m_width, m_height;
