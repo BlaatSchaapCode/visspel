18ab10a40281a9bc283f530aa415eb893387a3cd
André van Schoubroeck <andre@blaatschaap.be>
2022-04-01 14:57:48 +0200

    Fix build errors

--- src/types/map.cpp
+++ src/types/map.cpp
@@ -2,6 +2,6 @@
 #include "map.hpp"
 
 namespace types {
-types::Map::Map(GridSize w, GridSize h) : m_details(w,h), m_tiles(w,h) {
+types::Map::Map(GridSize w, GridSize h) : m_tiles(w,h), m_details(w,h)  {
 }
 } // namespace types
